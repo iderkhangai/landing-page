672b0373babd64abf39c2013dbdaa9070ded6317
unknown <iderhangai2014@gmail.com>
2022-03-22 18:36:35 +0100

    initial push  :: frond-end russian web

---
+++ assets/javascripts/blank.js
@@ -0,0 +1,13 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+    [5], { 357: function(n, o, p) { n.exports = p(16) } },
+    [
+        [357, 0]
+    ]
+]);
+document.documentElement.classList.remove('no-js');
+document.documentElement.classList.add('js');
+
+if (document.documentElement.style.setProperty) {
+    document.documentElement.style.setProperty('--viewport-height', window.innerHeight + 'px');
+    document.documentElement.style.setProperty('--viewport-height-actual', window.innerHeight + 'px');
+}
